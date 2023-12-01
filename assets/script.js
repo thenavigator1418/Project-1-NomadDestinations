1daaa2f466e53679969984b094d92adfd0b16f50
thenavigator1418 <henrydavis95@hotmail.co.uk>
2023-12-01 19:21:48 +0000

    added weatherapi function

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,29 @@
-// weatherapi call
-var weatherKey = "23140065d0524878b0c213950233011";
+// visualcrossing weather api call
+var queryURL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/buenosaires?unitGroup=metric&key=8N7VE7DU57F4PZWVAGHNN5ZVK&contentType=json";
+var weatherData = '';
+
+// fetch (queryURL)
+// .then (function (response) {
+//   return response.json();
+// }).then(function(data) {
+//   console.log(data);
+//   weatherData = JSON.stringify(data);
+// });
+
+function weatherApi() {
+    fetch(queryURL)
+        .then(function (response) {
+            return response.json();
+        }).then(function (data) {
+            console.log(data);
+              weatherData = JSON.stringify(data);
+            displayData();
+        });
+};
+
+function displayData() {
+    console.log(weatherData);
+};
+
+weatherApi();
+console.log(weatherData.latitude);
