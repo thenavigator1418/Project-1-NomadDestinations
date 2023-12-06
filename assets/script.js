069c44ff86b9858966516d389567dbec68d170ab
thenavigator1418 <henrydavis95@hotmail.co.uk>
2023-12-06 20:18:10 +0000

    adding recent js changes

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,17 @@
 // visualcrossing weather api call
-var queryURL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/buenosaires?unitGroup=metric&key=8N7VE7DU57F4PZWVAGHNN5ZVK&contentType=json";
+/* var queryURL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/buenosaires?unitGroup=metric&key=8N7VE7DU57F4PZWVAGHNN5ZVK&contentType=json";
 var weatherData = '';
-
-// fetch (queryURL)
-// .then (function (response) {
-//   return response.json();
-// }).then(function(data) {
-//   console.log(data);
-//   weatherData = JSON.stringify(data);
-// });
-
 function weatherApi() {
     fetch(queryURL)
         .then(function (response) {
             return response.json();
         }).then(function (data) {
-            console.log(data);
-              weatherData = JSON.stringify(data);
+            console.log(data.days[0].temp);
+            console.log(data.days[1].temp);
+            console.log(data.days[2].temp);
+            console.log(data.days[3].temp);
+            console.log(data.days[4].temp);
+              weatherData = (data);
             displayData();
         });
 };
@@ -26,4 +21,38 @@ function displayData() {
 };
 
 weatherApi();
-console.log(weatherData.latitude);
+console.log(weatherData.latitude); */
+
+
+
+var bnbData = "";
+const bnbUrl = 'https://airbnb13.p.rapidapi.com/search-location?location=Medellin&checkin=2024-06-01&checkout=2024-06-10&adults=1&children=0&infants=0&pets=0&page=1&currency=GBP';
+const bnbOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'e943522c53msh778f72f36525ee5p170a6ejsn25f1fadc0a94',
+		'X-RapidAPI-Host': 'airbnb13.p.rapidapi.com'
+	}
+};
+
+function bnbApi() {
+    fetch(bnbUrl, bnbOptions)
+        .then(function (response) {
+            return response.json();
+        }).then(function (data) {
+            console.log(data.results[0].images[0]);
+              bnbData = (data);
+            displayData();
+        });
+};
+
+function displayData() {
+    console.log(bnbData);
+};
+
+bnbApi();
+console.log(bnbData.results[0].images[0]);
+
+
+
+
