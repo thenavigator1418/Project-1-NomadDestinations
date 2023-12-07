ea970893fbc198bb46a7b99f79be72630d4c4712
Dija <145768761+dija-64@users.noreply.github.com>
2023-12-07 21:24:08 +0000

    ccreated reset.css file

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,27 @@
 // visualcrossing weather api call
 // queries API, console logs a five day forecast from current day
 function weatherApi(city, weatherLi) {
-    var queryURL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=8N7VE7DU57F4PZWVAGHNN5ZVK&contentType=json`;
-    var weatherData = "";
-    fetch(queryURL)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data.days[0].temp);
-        console.log(data.days[1].temp);
-        console.log(data.days[2].temp);
-        console.log(data.days[3].temp);
-        console.log(data.days[4].temp); // creates a five day forecast
-        weatherLi.empty();
-        for (let index = 0; index < 5; index++) {
-            weatherLi.append($(`<li class='d-inline'>${data.days[index].temp}</li>`));
-        }
-        weatherData = data;
-      });
-  }
-  
-var bnbData = "";
+  var queryURL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=8N7VE7DU57F4PZWVAGHNN5ZVK&contentType=json`;
+  var weatherData = "";
+  fetch(queryURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data.days[0].temp);
+      console.log(data.days[1].temp);
+      console.log(data.days[2].temp);
+      console.log(data.days[3].temp);
+      console.log(data.days[4].temp); // creates a five day forecast
+      weatherLi.empty();
+      for (let index = 0; index < 5; index++) {
+        weatherLi.append($(`<li class='d-inline'>${data.days[index].temp}</li>`));
+      }
+      weatherData = data;
+    });
+}
+
+/* var bnbData = "";
   
   function bnbApi(city, cityImg) { 
     const bnbUrl = `https://airbnb13.p.rapidapi.com/search-location?location=${city}&checkin=2024-06-01&checkout=2024-06-10&adults=1&children=0&infants=0&pets=0&page=1&currency=GBP`;
@@ -42,22 +42,22 @@ var bnbData = "";
         cityImg.attr("src", data.results[0].images[0]);
 
       });
-  } 
+  }  */
 
-  var cardsDiv = document.getElementById("cards"); // cardsDiv = all bootstrap cards on the page
+/* var cardsDiv = document.getElementById("cards"); // cardsDiv = all bootstrap cards on the page
 
-  cardsDiv.addEventListener("click", function (event) { // addEventListener for a click
-    var trimmedLocations = event.target.textContent.trim(); // trims the text content in card, which is the city name
-    var cityName = trimmedLocations.split(",", 2)[0]; // cityName is the trimmedLocations split at the ",", split into two elements, of which we select the first
-    console.log(cityName); 
-    var weatherId = cityName.replace(" ", "") + "Weather"; // Buenos Aires = BuenosAires
-    var weatherLi = $(`#${weatherId}`);
+cardsDiv.addEventListener("click", function (event) { // addEventListener for a click
+  var trimmedLocations = event.target.textContent.trim(); // trims the text content in card, which is the city name
+  var cityName = trimmedLocations.split(",", 2)[0]; // cityName is the trimmedLocations split at the ",", split into two elements, of which we select the first
+  console.log(cityName); 
+  var weatherId = cityName.replace(" ", "") + "Weather"; // Buenos Aires = BuenosAires
+  var weatherLi = $(`#${weatherId}`);
 
-    var cityImgId = cityName.replace(' ', '') + 'Img';
-    var cityImg = $(`#${cityImgId}`);
-    bnbApi(cityName, cityImg); // runs bnbApi func with cityName passed into template literal API search
-    weatherApi(cityName, weatherLi); // runs weatherApi func with cityName passed into template literal API search
-});
+  var cityImgId = cityName.replace(' ', '') + 'Img';
+  var cityImg = $(`#${cityImgId}`);
+  bnbApi(cityName, cityImg); // runs bnbApi func with cityName passed into template literal API search
+  weatherApi(cityName, weatherLi); // runs weatherApi func with cityName passed into template literal API search
+}); */
 
 
 
